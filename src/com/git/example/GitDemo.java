72cdf22789f34eef1c5edd8536238adc7676dea3
This is Prasad Bhalerao <prasadbhalerao2697@gmail.com>
2022-12-20 21:05:30 +0530

    first commit...

---
+++ src/com/git/example/GitDemo.java
@@ -0,0 +1,17 @@
+package com.git.example;
+
+public class GitDemo 
+{
+
+	public static void main(String[] args) 
+	{
+			String str="Hello";
+			
+			if(str.equals("Hello"))
+			{
+				System.out.println("It's Git Example");
+			}
+
+	}
+
+}
